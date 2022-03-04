77c6bde20b08ad55142a138902b522f6642bae19
Dana Larose <ywg.dana@gmail.com>
2022-03-03 21:31:19 -0600

    Initial import

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+static void RunFile(string path)
+{
+    byte[] bytes = File.ReadAllBytes(path);
+    
+}
+
+static void RunPrompt()
+{
+    for (;;)
+    {
+       Console.Write("> ");
+       var line = Console.ReadLine();
+       if (line.Length == 0)
+           break;
+       Console.WriteLine(line);
+    }
+}
+
+if (args.Length > 1)
+{
+    Console.WriteLine("Usage: cslox [script]");
+    return;
+}
+else if (args.Length == 1)
+{
+    Console.WriteLine(args[0]);
+}
+else
+{
+    RunPrompt();
+}
