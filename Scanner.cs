55f53a29e941fad2ea39c71ac8dd930e5e82f898
Dana Larose <dana.larose@lrsd.net>
2024-07-18 11:02:27 -0500

    Throw error in Scanner if we hit an unterminated numeric constant like 14.

--- Scanner.cs
+++ Scanner.cs
@@ -2,13 +2,13 @@ namespace CSLox;
 
 public class Scanner(string src)
 {
-    private readonly string source = src;
-    private readonly List<Token> tokens = [];
-    private int start;
-    private int current;
-    private int line = 1;
-   
-    static readonly Dictionary<string, TokenType> keywords = new()
+  private readonly string source = src;
+  private readonly List<Token> tokens = [];
+  private int start;
+  private int current;
+  private int line = 1;
+
+  static readonly Dictionary<string, TokenType> keywords = new()
     {
         { "and", TokenType.AND },
         { "class", TokenType.CLASS },
@@ -27,182 +27,189 @@ public class Scanner(string src)
         { "var", TokenType.VAR },
         { "while", TokenType.WHILE }
     };
-    
-    public List<Token> ScanTokens()
+
+  public List<Token> ScanTokens()
+  {
+    while (!IsAtEnd())
     {
-        while (!IsAtEnd())
+      start = current;
+      ScanToken();
+    }
+
+    return tokens;
+  }
+
+  private void ScanToken()
+  {
+    char c = Advance();
+    switch (c)
+    {
+      case '(':
+        AddToken(TokenType.LEFT_PAREN);
+        break;
+      case ')':
+        AddToken(TokenType.RIGHT_PAREN);
+        break;
+      case '{':
+        AddToken(TokenType.LEFT_BRACE);
+        break;
+      case '}':
+        AddToken(TokenType.RIGHT_BRACE);
+        break;
+      case ',':
+        AddToken(TokenType.COMMA);
+        break;
+      case '.':
+        AddToken(TokenType.DOT);
+        break;
+      case '-':
+        AddToken(TokenType.MINUS);
+        break;
+      case '+':
+        AddToken(TokenType.PLUS);
+        break;
+      case ';':
+        AddToken(TokenType.SEMICOLON);
+        break;
+      case '*':
+        AddToken(TokenType.STAR);
+        break;
+      case '!':
+        AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+        break;
+      case '=':
+        AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+        break;
+      case '<':
+        AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+        break;
+      case '>':
+        AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+        break;
+      case '/':
+        if (Match('/'))
         {
-            start = current;
-            ScanToken();
-        }
-
-        return tokens;
-    }
-
-    private void ScanToken()
-    {
-        char c = Advance();
-        switch (c)
-        {
-            case '(': 
-                AddToken(TokenType.LEFT_PAREN);
-                break;
-            case ')':
-                AddToken(TokenType.RIGHT_PAREN);
-                break;
-            case '{':
-                AddToken(TokenType.LEFT_BRACE);
-                break;
-            case '}':
-                AddToken(TokenType.RIGHT_BRACE);
-                break;
-            case ',':
-                AddToken(TokenType.COMMA);
-                break;
-            case '.':
-                AddToken(TokenType.DOT);
-                break;
-            case '-':
-                AddToken(TokenType.MINUS);
-                break;
-            case '+':
-                AddToken(TokenType.PLUS);
-                break;
-            case ';':
-                AddToken(TokenType.SEMICOLON);
-                break;
-            case '*':
-                AddToken(TokenType.STAR);
-                break;
-            case '!':
-                AddToken(Match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
-                break;
-            case '=':
-                AddToken(Match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
-                break;
-            case '<':
-                AddToken(Match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
-                break;
-            case '>':
-                AddToken(Match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
-                break;
-            case '/':
-                if (Match('/'))
-                {
-                    while (Peek() != '\n' && !IsAtEnd())
-                        Advance();
-                }
-                else
-                {
-                    AddToken(TokenType.SLASH);
-                }
-                
-                break;
-            case ' ':
-            case '\r':
-            case '\t':
-                break;
-            case '\n':
-                line++;
-                break;
-            case '"':
-                String();
-                break;
-            default:
-                if (IsDigit(c))
-                    Number();
-                else if (IsAlpha(c))
-                    Identifier();
-                else
-                    Lox.Error(line, $"Unexpected character: {c}.");
-
-                break;
-        }
-    }
-
-    private void Identifier()
-    {
-        while (IsAlphaNumeric(Peek()))
-            Advance();
-
-        string text = source.Substring(start, current - start - 1);
-        TokenType type = keywords.ContainsKey(text) ? keywords[text] : TokenType.IDENTIFIER;
-        AddToken(type);
-    }
-    
-    private static bool IsAlpha(char c) => char.IsLetter(c) || c == '_';
-    private static bool IsAlphaNumeric(char c) => IsAlpha(c) || IsDigit(c);    
-    private static bool IsDigit(char c) => c >= '0' && c <= '9';
-
-    private void ConsumeDigits()
-    {
-        while (IsDigit(Peek()))
-            Advance();
-    }
-    
-    private void Number()
-    {
-        ConsumeDigits();
-        if (Peek() == '.' && IsDigit(Peeek()))
-        {
-            Advance();
-            ConsumeDigits();
-        }
-
-        AddToken(TokenType.NUMBER, double.Parse(source[start..current]));
-    }
-    
-    private void String()
-    {
-        while (Peek() != '"' && !IsAtEnd())
-        {
-            if (Peek() == '\n')
-                line++;
+          while (Peek() != '\n' && !IsAtEnd())
             Advance();
         }
-
-        if (IsAtEnd())
+        else
         {
-            Lox.Error(line, "Unterminated string.");
-            return;
+          AddToken(TokenType.SLASH);
         }
-        
-        // skip closing "
-        Advance();
 
-        string? value = source.Substring(start + 1, current - start - 2);
-        AddToken(TokenType.STRING, value);
+        break;
+      case ' ':
+      case '\r':
+      case '\t':
+        break;
+      case '\n':
+        line++;
+        break;
+      case '"':
+        String();
+        break;
+      default:
+        if (IsDigit(c))
+          Number();
+        else if (IsAlpha(c))
+          Identifier();
+        else
+          Lox.Error(line, $"Unexpected character: {c}.");
+
+        break;
     }
+  }
 
-    private char Peek() => IsAtEnd() ? '\0' : source[current];
+  private void Identifier()
+  {
+    while (IsAlphaNumeric(Peek()))
+      Advance();
 
-    private char Peeek()
+    string text = source.Substring(start, current - start - 1);
+    TokenType type = keywords.ContainsKey(text) ? keywords[text] : TokenType.IDENTIFIER;
+    AddToken(type);
+  }
+
+  private static bool IsAlpha(char c) => char.IsLetter(c) || c == '_';
+  private static bool IsAlphaNumeric(char c) => IsAlpha(c) || IsDigit(c);
+  private static bool IsDigit(char c) => c >= '0' && c <= '9';
+
+  private void ConsumeDigits()
+  {
+    while (IsDigit(Peek()))
+      Advance();
+  }
+
+  private void Number()
+  {
+    ConsumeDigits();
+
+    if (Peek() == '.')
     {
-        if (current + 1 >= source.Length)
-            return '\0';
+      if (!IsDigit(Peeek()))
+      {
+        Lox.Error(line, "Invalid numeric constant.");
+        return;
+      }
 
-        return source[current + 1];
+      Advance();
+      ConsumeDigits();
     }
-    
-    private bool Match(char expected)
+
+    AddToken(TokenType.NUMBER, double.Parse(source[start..current]));
+  }
+
+  private void String()
+  {
+    while (Peek() != '"' && !IsAtEnd())
     {
-        if (IsAtEnd())
-            return false;
-        if (source[current] != expected)
-            return false;
-
-        current++;
-
-        return true;
+      if (Peek() == '\n')
+        line++;
+      Advance();
     }
-    
-    private char Advance() => source[current++];
 
-    private void AddToken(TokenType type, object? literal = null)
+    if (IsAtEnd())
     {
-        string text = source[start..current];
-        tokens.Add(new(type, text, literal, line));
+      Lox.Error(line, "Unterminated string.");
+      return;
     }
-    
-    private bool IsAtEnd() => current >= source.Length;
+
+    // skip closing "
+    Advance();
+
+    string? value = source.Substring(start + 1, current - start - 2);
+    AddToken(TokenType.STRING, value);
+  }
+
+  private char Peek() => IsAtEnd() ? '\0' : source[current];
+
+  private char Peeek()
+  {
+    if (current + 1 >= source.Length)
+      return '\0';
+
+    return source[current + 1];
+  }
+
+  private bool Match(char expected)
+  {
+    if (IsAtEnd())
+      return false;
+    if (source[current] != expected)
+      return false;
+
+    current++;
+
+    return true;
+  }
+
+  private char Advance() => source[current++];
+
+  private void AddToken(TokenType type, object? literal = null)
+  {
+    string text = source[start..current];
+    tokens.Add(new(type, text, literal, line));
+  }
+
+  private bool IsAtEnd() => current >= source.Length;
 }
