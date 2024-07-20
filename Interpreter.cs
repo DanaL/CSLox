699ae9911db2fed322f9256a56dbd03ce54ff2bc
Dana Larose <ywg.dana@gmail.com>
2024-07-19 23:18:32 -0500

    Basic interpreter functionality

---
+++ Interpreter.cs
@@ -0,0 +1,85 @@
+using System.Runtime.InteropServices;
+
+namespace CSLox;
+
+class Interpreter : IVisitor<object?>
+{
+  object? Evaluate(Expr expr) => expr.Accept(this);
+  
+  static bool IsEqual(object? a, object? b)
+  {
+    if (a is null && b is null)
+      return true;
+    if (a == null)
+      return false;
+
+    return a.Equals(b);
+  }
+
+  static bool IsTruthy(object? obj)
+  {
+    if (obj is null)
+      return false;
+    if (obj is bool b)
+      return b;
+
+    return true;
+  }
+
+  public object? VisitBinaryExpr(Binary expr)
+  {
+    object? left = Evaluate(expr.Left);
+    object? right = Evaluate(expr.Right);
+
+    switch (expr.Op.Type)
+    {
+      case TokenType.MINUS:
+        return (double)left - (double)right;
+      case TokenType.SLASH:
+        return (double)left / (double)right;
+      case TokenType.STAR:
+        return (double)left * (double)right;
+      case TokenType.PLUS:
+        if (left is double lval && right is double rval)
+          return lval + rval;
+
+        if (left is string && right is string)
+          return $"{left}{right}";
+        break;
+      case TokenType.GREATER:
+        return (double)left > (double)right;
+      case TokenType.GREATER_EQUAL:
+        return (double)left >= (double)right;
+      case TokenType.LESS:
+        return (double)left < (double)right;
+      case TokenType.LESS_EQUAL:
+        return (double)left <= (double)right;
+      case TokenType.EQUAL_EQUAL:
+        return IsEqual(left, right);
+      case TokenType.BANG_EQUAL:
+        return !IsEqual(left, right);
+    }
+    
+    return null;
+  }
+
+  public object? VisitGroupingExpr(Grouping expr) => Evaluate(expr.Expression);
+  public object? VisitLiteralExpr(Literal expr) => expr.Value;
+
+  public object? VisitTernaryExpr(Ternary expr)
+  {
+    throw new NotImplementedException();
+  }
+
+  public object? VisitUnaryExpr(Unary expr)
+  {
+    object? right = Evaluate(expr.Right);
+
+    return expr.Op.Type switch
+    {
+      TokenType.MINUS => -(double)right,
+      TokenType.BANG => IsTruthy(right),
+      _ => null
+    };
+  }
+}
